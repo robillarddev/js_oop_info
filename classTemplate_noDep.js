49919c6c63a685b27e5afa854ace0d55b39bcb79
Morgan Yarbrough <myarbrough@bluewave-computing.com>
2015-01-29 06:48:01 -0500

    added no dep template

---
+++ classTemplate_noDep.js
@@ -0,0 +1,23 @@
+var className = (function() {
+    //https://github.com/sevin7676/js_oop_info/blob/master/classTemplate_noDep.js
+    var local = this;
+
+    var Class = function() {
+        var _sf = {}, sf = this;        
+        (function Private() {}).call(_sf);
+        (function Public() {}).call(sf);
+    };
+
+    (function Prototype() {
+        //this.demo = function() {var sf = this;};
+    }).call(Class.prototype);
+
+    (function Static() {
+        var _sf = local,
+            sf = Class;
+        (function Private() {}).call(local);
+        (function Public() {}).call(Class);
+    })();
+    
+    return Class;
+}());
